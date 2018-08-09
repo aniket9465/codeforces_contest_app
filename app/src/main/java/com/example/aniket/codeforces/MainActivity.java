14c59caf422d9e654c7b96fc96c70f2bc4840d18
aniket <aniket14goyal@gmail.com>
2018-08-09 18:56:26 +0530

    timer started

--- app/src/main/java/com/example/aniket/codeforces/MainActivity.java
+++ app/src/main/java/com/example/aniket/codeforces/MainActivity.java
@@ -16,6 +16,8 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import android.os.Handler;
+
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -33,39 +35,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        contestlist=(RecyclerView)findViewById(R.id.list);
-        Retrofit retrofit=new Retrofit.Builder().baseUrl("http://codeforces.com/api/").addConverterFactory(GsonConverterFactory.create()).build();
-        api_interface apiservice=retrofit.create(api_interface.class);
-        Call<Contests> call = apiservice.get_contests();
-        ra=new Radapter(list);
-        contestlist.setAdapter(ra);
-        final LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        contestlist.setLayoutManager(layoutManager);
-        call.enqueue(new Callback<Contests>() {
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
             @Override
-            public void onResponse(Call<Contests> call, Response<Contests> response) {
-                result[] res=response.body().getResult();
-                for(int i=0;i<Array.getLength(res);i++)
-                {
-                    if(res[i].getPhase().equals("BEFORE"))
-                    {
-                       list.add(new Pair<>((res[i].getName()),(abs((Long.parseLong(res[i].getRelativeTimeSeconds())/(3600*24)))+" days "+abs((Long.parseLong(res[i].getRelativeTimeSeconds())%(3600*24))/3600)+" hours "+abs((Long.parseLong(res[i].getRelativeTimeSeconds())%(3600))/60)+" minutes\n")));
+            public void run() {
+                Log.d("hi","hi");
+                contestlist = (RecyclerView) findViewById(R.id.list);
+                Retrofit retrofit = new Retrofit.Builder().baseUrl("http://codeforces.com/api/").addConverterFactory(GsonConverterFactory.create()).build();
+                api_interface apiservice = retrofit.create(api_interface.class);
+                Call<Contests> call = apiservice.get_contests();
+                ra = new Radapter(list);
+                contestlist.setAdapter(ra);
+                final LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+                layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+                contestlist.setLayoutManager(layoutManager);
+                call.enqueue(new Callback<Contests>() {
+                    @Override
+                    public void onResponse(Call<Contests> call, Response<Contests> response) {
+                        list.clear();
+                        result[] res = response.body().getResult();
+                        for (int i = 0; i < Array.getLength(res); i++) {
+                            if (res[i].getPhase().equals("BEFORE")) {
+                                list.add(new Pair<>((res[i].getName()), res[i].getRelativeTimeSeconds()));
+                            }
+
+                        }
+                        Collections.reverse(list);
+                        ra.notifyData(list);
+
                     }
 
-                }
-                Collections.reverse(list);
-                ra.notifyData(list);
+                    @Override
+                    public void onFailure(Call<Contests> call, Throwable t) {
+                        Log.d("TAG", "failed");
+                    }
+                });
 
             }
-
-            @Override
-            public void onFailure(Call<Contests> call, Throwable t) {
-                Log.d("TAG","failed");
-            }
-        });
-       // contests.setText(contest_list);
+        },100);
+            setContentView(R.layout.activity_main);
     }
     public class Radapter extends RecyclerView.Adapter<Radapter.itemviewholder>
     {
@@ -94,10 +102,21 @@ public class MainActivity extends AppCompatActivity {
             return this.list.size();
         }
         @Override
-        public void onBindViewHolder(itemviewholder holder, int position) {
+        public void onBindViewHolder(final itemviewholder holder, final int position) {
             Log.d("TAG","bind");
             holder.timeleft.setText(list.get(position).second);
             holder.contestname.setText(list.get(position).first);
+            final Handler handler =new Handler();
+            handler.postDelayed(new Runnable() {
+                int i=0;
+                @Override
+                public void run() {
+                    Long tt=Long.parseLong(list.get(position).second)+i;
+                    ++i;
+                    holder.timeleft.setText((abs(tt / (3600 * 24)) + " days " + abs((tt % (3600 * 24)) / 3600) + " hours " + abs(((tt) % (3600)) / 60) + " minutes  "+abs((tt % (60)) ) + " seconds"));
+                    handler.postDelayed(this,1000);
+                }
+            },100);
         }
 
         public class itemviewholder extends RecyclerView.ViewHolder
@@ -109,6 +128,7 @@ public class MainActivity extends AppCompatActivity {
                 super(parent);
                 contestname=(TextView)parent.findViewById(R.id.contestname);
                 timeleft=(TextView)parent.findViewById(R.id.timeleft);
+
             }
         }
     }
